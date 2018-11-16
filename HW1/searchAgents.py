7789b31462555a9ca5390eac436eee02892dea5f
saminnayeri <samin.nayeri@yahoo.com>
2018-11-16 23:17:43 +0330

    Update searchAgents.py
    all done

--- HW1/searchAgents.py
+++ HW1/searchAgents.py
@@ -485,7 +485,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.breadthFirstSearch(problem)
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -521,7 +521,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 def mazeDistance(point1, point2, gameState):
     """
